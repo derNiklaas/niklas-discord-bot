261945d62c1ace1654e61b44bf6c2f1ae82a6cb0
Niklas <derNiklaas@users.noreply.github.com>
2020-07-31 17:58:18 +0200

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,121 @@
+import {Client, GuildMember, Message, MessageEmbed, MessageReaction, PartialUser, TextChannel, User} from "discord.js";
+import request from 'request';
+
+const client: Client = new Client({partials: ['CHANNEL', 'MESSAGE', 'REACTION']});
+const reactableEmotes = ["rainbowsheepgif"];
+const TWITCH_ROLE_ID: string = "622830603342970901";
+const ROLES_CHANNEL_ID: string = "686356862324834305";
+const LIVE_CHANNEL_ID: string = "497003072288194580";
+
+let lastLiveNotification: Message;
+require('dotenv').config();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+    setInterval(checkTwitchStatus, 60000);
+});
+
+client.on('messageReactionAdd', (reaction: MessageReaction, user: User | PartialUser) => {
+    if (!reactableEmotes.includes(reaction.emoji.name)) {
+        return;
+    }
+    if (reaction.message.channel.id !== ROLES_CHANNEL_ID) {
+        return;
+    }
+    const member: GuildMember = reaction.message.guild.member(user.id);
+
+    if (!member.roles.cache.has(TWITCH_ROLE_ID)) {
+        member.roles.add(TWITCH_ROLE_ID).then();
+    }
+});
+
+client.on('messageReactionRemove', (reaction: MessageReaction, user: User | PartialUser) => {
+    if (!reactableEmotes.includes(reaction.emoji.name)) {
+        return;
+    }
+    if (reaction.message.channel.id !== ROLES_CHANNEL_ID) {
+        return;
+    }
+    const member: GuildMember = reaction.message.guild.member(user.id);
+
+    if (member.roles.cache.has(TWITCH_ROLE_ID)) {
+        member.roles.remove(TWITCH_ROLE_ID).then();
+    }
+});
+
+
+function checkTwitchStatus() {
+    request("https://api.twitch.tv/helix/streams?user_login=derniklaas", {
+        headers: {
+            "Client-ID": process.env.TWITCH_CLIENT_ID,
+            "Authorization": "Bearer " + process.env.TWITCH_TOKEN
+        }
+    }, async (error, httpResponse, body) => {
+        if (error) {
+            console.log("error: " + error);
+            return;
+        }
+        let data = JSON.parse(body);
+        data = data.data[0];
+        if (data && data.type === "live") {
+            const test = client.channels.cache.get(LIVE_CHANNEL_ID) as TextChannel;
+            const game = await getGameFromID(data.game_id);
+            const viewer = data.viewer_count;
+            const width = 600;
+            const thumbnail = data.thumbnail_url.replace("{width}", width).replace("{height}", Math.round(width / 16 * 9));
+            if (lastLiveNotification) {
+                lastLiveNotification.edit(`<@&${TWITCH_ROLE_ID}>: Niklas ist jetzt live`, buildEmbed(data.title, game, viewer, thumbnail)).then();
+            } else {
+                lastLiveNotification = await test.send(`<@&${TWITCH_ROLE_ID}>: Niklas ist jetzt live`, buildEmbed(data.title, game, viewer, thumbnail));
+            }
+        } else {
+            lastLiveNotification.delete().then();
+            lastLiveNotification = undefined;
+        }
+    });
+}
+
+function buildEmbed(title: string, game: string, viewer: string, thumbnail: string): MessageEmbed {
+    const embed: MessageEmbed = new MessageEmbed();
+    embed.setColor("#9400D3");
+    embed.setAuthor("derNiklaas", "https://cdn.discordapp.com/avatars/153113441429749760/a_5d47b975cfdd39ca9f82be920008958d.webp", "https://www.twitch.tv/derNiklaas");
+    embed.setTitle(title);
+    embed.setURL("https://www.twitch.tv/derNiklaas");
+    embed.addField("Kategorie", game, true);
+    embed.addField("Zuschauer", viewer, true);
+    embed.setImage(thumbnail);
+
+    return embed;
+}
+
+async function getGameFromID(id: string): Promise<string> {
+    let data = undefined;
+    request('https://api.twitch.tv/helix/games?id=' + id, {
+        headers: {
+            "Client-ID": process.env.TWITCH_CLIENT_ID,
+            "Authorization": "Bearer " + process.env.TWITCH_TOKEN
+        }
+    }, (error, httpResponse, body) => {
+        if (error) {
+            console.log("error fetching game");
+        }
+        const json = JSON.parse(body);
+        if (json.data.length === 0) {
+            data = "";
+        } else {
+            data = json.data[0].name;
+        }
+    });
+
+    while (!data) {
+        await sleep(10);
+    }
+
+    return data;
+}
+
+function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+client.login(process.env.DISCORD_TOKEN).then();
