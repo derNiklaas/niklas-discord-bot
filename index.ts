d616620d751415a6536005dfe45d015b48f6a645
Niklas <derNiklaas@users.noreply.github.com>
2020-10-31 16:09:37 +0100

    Beautified the last update time.
    Example:
    3:4:5 -> 03:04:05

--- index.ts
+++ index.ts
@@ -120,7 +120,11 @@ function buildEmbed(streamInfo: object): MessageEmbed {
         tries = `Offline? Noch ${reconnectTries} Versuche.`;
     }
 
-    embed.setFooter(`Letztes Update: ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} ${tries}`);
+    const hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+    const minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    const seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+
+    embed.setFooter(`Letztes Update: ${hours}:${minutes}:${seconds} ${tries}`);
 
     return embed;
 }
